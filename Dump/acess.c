ff959004cd47682befa41c24f3ffe150b204a746
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-02-27 23:12:28 +0700

    Create acess.c

---
+++ Dump/acess.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdin.h>
+#include <dump.h>
+#include <process.h>
+int main(Eval) {
+Eval($dkdjdhdksjhjDjdjsljshakjshdjdkdifhjxpajsopajdhlshsgdkakkdhdpajshs544676764488858dkshhakahdjaohEpqihapsjdhfosphagfdzcgzhzgsisohsgsoqusgshkaiwgshsjjsjshgdajshdosuwgq564646949495959949595*466sjgsfisgsfshaigwfwfuaihjsgKshdgsjkshshSHSGSPAOHFEJKGAPAROOTDJHHPENHCSSKHHTMLSJJDHHSJjdjdhjajskspp)))
+return Eval
+}
+int main(Zlib) {
+Zlib($shahjjJhshshshjshdhKzjdgskjsgskahsgskudhdhshfkgohssh5646464646)
+return Zlib
+}
+reup Eval
+reup Zlib
+}
+  }
+done
+done
+done
+close_f()
