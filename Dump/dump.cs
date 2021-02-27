5fa7c40c69951d602335f31e1a62eea1fbb050fe
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-02-27 23:19:03 +0700

    Create dump.cs

---
+++ Dump/dump.cs
@@ -0,0 +1,15 @@
+#Windows(main)
+#Windows(main)
+#Windows(main)
+windows main(Xlib) {
+Xlib.decdec($KSSHHALjhudhdjakkajshsllixhdhxbbcsjkafgjsgduspaytsyaoafsvekslsgoxgdksygdkjgs46467637345544554646LSHSAOYDAYSUPYWTjkkaRootCsshtmlshsgjKhhhjjhgWindowsJdghdlaosgdopsyagelshHhgdgsjj)
+exec Xlib
+}
+windows main(ZEval) {
+ZEval.executable.stdout($/0/0/0/0/0/0//0/0/0/0/0/0//0/0/0/0/0/0/0/0/0//0/0/0/0/0/0/0/8/8/8/5//5/4/8/8/4//8/8/5/5/4/5/545////0//5/5/4/5/'//5/4/5/5/8/5/5/5/7//5/5/4/5/5/5/5/5/5//4/5//8/5/5/8/5/5/8/5/8/REMITEACESS56466/0//0/0/0/0/0/0/0/0/0/0/8//5/5/7//0//0//0/0/0/0/0//0/0/0/0/0/0//0/0/8/8/8/1//8/8/5/5/)
+exec ZEval
+}
+tap Xlib
+tap ZEval
+}
+close_exit()
