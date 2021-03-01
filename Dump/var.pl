9d4f4f60da105ba27fd23d19bf5256f05c104c52
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-03-01 22:37:48 +0700

    Create var.pl

---
+++ Dump/var.pl
@@ -0,0 +1,6 @@
+#!/perl5
+use 6.0.0
+use strict
+use warning
+use marshalperl2
+exec(marshalperl2.loads(c\x00\x86\x64\SEVEKSKSGALHZSHALSHSKLSGSHSLSEBALJDHDEVALZIPRARTARAJDHHDSLDHHDLDHDksvdwpsjvdlwpshdshldgdk2gCgelsjsjdhd&lsjwhsodjehowpwhdgdowhavskl2skdpwgdvdowhsvslwovspewo4948168195549/5+5-4-5*4/5'/5*5=5/*5*5/4*4646465918845131664*4/4*5-+4+5/5/5@5@$5$5-5/56565*5/5/$$$$4$5*5*5*5*5**5*5*191694644'===5*464/8@5/5**5*5*5*5686*5*4+684664+5646\xbyte\SCERIPAJSGDLSHVkdgwlsvwalsvfdlschslavXlwlzgwlagswpjsgsgjsos*vsks49446679494//-5=4=5=5/4/,5-5!5!5=5?5!'5"5=8/5/45/"8=5:"8'5:5#4/8/5#4#4#5"5/5'5/5*-5/?5"9=5'5"/5/5/\xselesai''')))
