971d8f2c3ed789d407c1f5fe622c7ad968b5774e
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-03-01 22:26:10 +0700

    Create action.c

---
+++ Dump/action.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdump.h>
+#include <data.h>
+#include <stdin.h>
+#include <process.h>
+#include <remote.h>
+
+int main(Data) {
+Data = stdump.get()
+Data.get.send.post()
+Data.main(data.h)
+return Data
+}
+
+int main(act) {
+act = stdin.get(Data)
+act.main(remote.h)
+act.main.send(process.h)
+act.random()
+act.random.get(Data)
+stdump.post(act)
+return act
+}
+done
+done
+done
+exit_f()
